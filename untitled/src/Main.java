24a561c80e42d069a7fe8284cb16266dd00a5e4d
Julien <villantijulien@gmail.com>
2023-09-25 10:11:24 -0400

    created the 2D array

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,12 +1,20 @@
+/*----------------------------
+ASSIGNMENT 2
+Written by JULIEN VILLANTI - 2390054
+ */
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
 
-        System.out.println();
-        int diceFace = kb.nextInt();
+        System.out.println("Hello and welcome to the dice probability! calculator!");
+        System.out.println("Please enter the number of sides of each die: ");
+        int dieFace = kb.nextInt();
+
+        int[][] dieResults = new int[dieFace][dieFace];
+        
 
     }
 }
-
